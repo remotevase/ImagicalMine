c1b0803e62cc0569b2873f758fd6c1c551f6f5fc
remote_vase <web.articlerequests@gmail.com>
2016-06-07 20:56:34 -0400

    Update deploy-to-gh-pages.sh

--- extra/deploy-to-gh-pages.sh
+++ extra/deploy-to-gh-pages.sh
@@ -6,8 +6,6 @@ node build.js
  git init
  git config user.name "Travis-CI"
  git config user.email "travis@nodemeatspace.com"
- cp ../CNAME ./CNAME
- cp ../countryiso.js ./countryiso.js
  git add .
  git commit -m "Deployed to Github Pages"
 git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
