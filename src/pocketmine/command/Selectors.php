29fa005c51e25246e0dcfb176f26b3cb4b42f102
Ad5001 <ad5001yt@gmail.com>
2016-06-13 18:52:36 +0200

    Staring selectors...

---
+++ src/pocketmine/command/Selectors.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * src/pocketmine/command/Selectors.php
+ *
+ * @package default
+ */
+
+
+/*
+ *
+ *  _                       _           _ __  __ _
+ * (_)                     (_)         | |  \/  (_)
+ *  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___
+ * | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \
+ * | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/
+ * |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___|
+ *                     __/ |
+ *                    |___/
+ *
+ * This program is a third party build by ImagicalMine.
+ *
+ * PocketMine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author ImagicalMine Team
+ * @link http://forums.imagicalcorp.ml/
+ *
+ *
+*/
+
+namespace pocketmine\command;
+class Selectors {
+    /*
+    This is executed when executing a command
+    */
+    public static function add(string $selectorName, Selectors $select) {
+        
+    }
+}
