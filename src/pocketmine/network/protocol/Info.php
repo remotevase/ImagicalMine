7bdee4da030b5afc0d012c085192f22e11057826
remote_vase <web.articlerequests@gmail.com>
2016-06-13 16:53:49 -0400

    Due to packet changes, nothing below 14.3 can be accepted

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -36,7 +36,7 @@ interface Info
      * Actual Minecraft: PE protocol version
      */
     const CURRENT_PROTOCOL = 82;
-    const ACCEPTED_PROTOCOLS = [82];
+    const ACCEPTED_PROTOCOLS = [70, 82];
     const LOGIN_PACKET = 0x01;
     const PLAY_STATUS_PACKET = 0x02;
     const DISCONNECT_PACKET = 0x05;
